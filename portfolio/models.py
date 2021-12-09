1c1d8786ec9e7af2dd03cd29cc05dae47b600299
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-09 20:48:31 +0100

    Add models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(blank=True, null=True, upload_to="projects")
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(blank=True, null=True, upload_to="skills")
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.title
