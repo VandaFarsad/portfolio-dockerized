e65cb44539c37518cc10ab48600cf37550cb2f08
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-09 21:36:50 +0100

    Add scroll up script to app.js

--- portfolio/static/js/app.js
+++ portfolio/static/js/app.js
@@ -3,6 +3,9 @@ const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
 
+// scroll to top selection
+const scrollUp = document.querySelector("#scroll-up");
+
 // select nav links
 const navLink = document.querySelectorAll(".nav-link");
 
@@ -17,3 +20,12 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+// scroll to top functionality
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
