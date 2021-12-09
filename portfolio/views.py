2a39b631a7aeb0993ca2a56af98458b28d4bcf7b
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-09 20:52:05 +0100

    Add index and impressum view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.db.models.query import QuerySet
+from django.views.generic import TemplateView
 
-# Create your views here.
+from .models import Project, Skill
+
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        skills = Skill.objects.filter(is_active=True)
+        projects = Project.objects.filter(is_active=True)
+
+        context["skills"] = skills
+        context["projects"] = projects
+
+        return context
+
+
+class ImpressumView(TemplateView):
+    template_name = "impressum.html"
