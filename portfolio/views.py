45acf4a867904453a92038d3a684ea4953f35773
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-09 20:54:11 +0100

    Add splitting method to index view

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,14 +7,18 @@ from .models import Project, Skill
 class IndexView(TemplateView):
     template_name = "index.html"
 
+    def _split_into_blocks(self, objects: QuerySet, sublist_len: int = 3) -> list:
+        """Splitting a list into a nested list, where each sub list has a fixed length."""
+        return [objects[i:i+sublist_len] for i in range(0, len(objects), sublist_len)]
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
         skills = Skill.objects.filter(is_active=True)
         projects = Project.objects.filter(is_active=True)
 
-        context["skills"] = skills
-        context["projects"] = projects
+        context["skills"] = self._split_into_blocks(skills, 3)
+        context["projects"] = self._split_into_blocks(projects, 2)
 
         return context
 
