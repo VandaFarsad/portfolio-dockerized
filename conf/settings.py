189b3fb1a6f96e2f9135f384ea004435eff3512b
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-10 11:19:16 +0100

    Add media url and root and move static folder

--- conf/settings.py
+++ conf/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (str(BASE_DIR.joinpath('portfolio/static')),)
+STATIC_ROOT = BASE_DIR / 'data/static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'data/media'
 
 
 # Default primary key field type
