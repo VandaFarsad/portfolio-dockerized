fe3f7e643c0f3e020753ed0c1973dce68beefe38
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-12 21:19:24 +0100

    Add constance and make about me customizable

--- conf/settings.py
+++ conf/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'constance.backends.database',
+    "constance",  # must be before project apps
+
     "portfolio",
     "core",
 ]
@@ -130,3 +134,14 @@ MEDIA_ROOT = BASE_DIR / "portfolio/data/media"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# django-constance
+# https://django-constance.readthedocs.io/en/latest/index.html
+
+CONSTANCE_BACKEND = "constance.backends.database.DatabaseBackend"
+
+CONSTANCE_CONFIG = {
+    "ABOUT_ME": ("Tell something about yourself.", "About me text field"),
+    "MORE_ABOUT_ME": ("Tell even more about yourself.", "More about me text field"),
+}
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.db.models.query import QuerySet
 from django.views.generic import TemplateView
+from constance import config
+
 
 from .models import Project, Skill
 
@@ -9,7 +11,9 @@ class IndexView(TemplateView):
 
     def _split_into_blocks(self, objects: QuerySet, sublist_len: int = 3) -> list:
         """Splitting a list into a nested list, where each sub list has a fixed length."""
-        return [objects[i:i+sublist_len] for i in range(0, len(objects), sublist_len)]
+        return [
+            objects[i : i + sublist_len] for i in range(0, len(objects), sublist_len)
+        ]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -19,6 +23,8 @@ class IndexView(TemplateView):
 
         context["skills"] = self._split_into_blocks(skills, 3)
         context["projects"] = self._split_into_blocks(projects, 2)
+        context["about_me"] = config.ABOUT_ME
+        context["more_about_me"] = config.MORE_ABOUT_ME
 
         return context
 
