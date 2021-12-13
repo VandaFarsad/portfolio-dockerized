228d42d3590d68247021d607792651818eeaa22b
Vanda Farsad <vanda.farsad@posteo.de>
2021-12-13 09:48:35 +0100

    Add add paragraphs in templates with constance str

--- portfolio/views.py
+++ portfolio/views.py
@@ -15,6 +15,10 @@ class IndexView(TemplateView):
             objects[i : i + sublist_len] for i in range(0, len(objects), sublist_len)
         ]
 
+    def _split_paragraphs(self, content: str) -> list[str]:
+        """Use this method and a for loop to add paragraphs in the template when using constance text fields."""
+        return content.split("\p")
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
@@ -24,7 +28,7 @@ class IndexView(TemplateView):
         context["skills"] = self._split_into_blocks(skills, 3)
         context["projects"] = self._split_into_blocks(projects, 2)
         context["about_me"] = config.ABOUT_ME
-        context["more_about_me"] = config.MORE_ABOUT_ME
+        context["more_about_me"] = self._split_paragraphs(config.MORE_ABOUT_ME)
 
         return context
 
