2d7e6e833e12442d1e94e64e2a1e61113b9d6be9
vandaf <vanda.farsad@posteo.de>
2022-07-12 18:10:03 +0200

    cheat statics

--- conf/urls.py
+++ conf/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
-if settings.DEBUG:
+# if settings.DEBUG:
+if True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
