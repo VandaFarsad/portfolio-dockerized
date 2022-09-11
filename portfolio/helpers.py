84a219487810bf5ad9bca659253c4d9b41f5a7d1
Vanda Farsad <contact@initial-commit.com>
2022-09-11 14:12:15 +0200

    mv split methos into helpers.py

---
+++ portfolio/helpers.py
@@ -0,0 +1,11 @@
+from django.db.models.query import QuerySet
+
+
+def split_into_blocks(objects: QuerySet, sublist_len: int = 3) -> list:
+    """Splitting a list into a nested list, where each sub list has a fixed length."""
+    return [objects[i : i + sublist_len] for i in range(0, len(objects), sublist_len)]  # noqa
+
+
+def split_paragraphs(content: str) -> list[str]:
+    """Use this method and a for loop to add paragraphs in the template when using constance text fields."""
+    return content.split(r"\p")
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,22 +1,12 @@
-from django.db.models.query import QuerySet
-from django.views.generic import TemplateView
 from constance import config
+from django.views.generic import TemplateView
 
+from helpers import split_into_blocks, split_paragraphs
 
 from .models import Project, Skill
 
 
-class BaseView(TemplateView):
-    def _split_into_blocks(self, objects: QuerySet, sublist_len: int = 3) -> list:
-        """Splitting a list into a nested list, where each sub list has a fixed length."""
-        return [objects[i : i + sublist_len] for i in range(0, len(objects), sublist_len)]  # noqa
-
-    def _split_paragraphs(self, content: str) -> list[str]:
-        """Use this method and a for loop to add paragraphs in the template when using constance text fields."""
-        return content.split(r"\p")
-
-
-class IndexView(BaseView):
+class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
@@ -25,13 +15,13 @@ class IndexView(BaseView):
         skills = Skill.objects.filter(is_active=True)
         projects = Project.objects.filter(is_active=True)
 
-        context["skills"] = self._split_into_blocks(skills, 3)
-        context["projects"] = self._split_into_blocks(projects, 2)
+        context["skills"] = split_into_blocks(skills, 3)
+        context["projects"] = split_into_blocks(projects, 2)
         context["about_me"] = config.ABOUT_ME
-        context["more_about_me"] = self._split_paragraphs(config.MORE_ABOUT_ME)
+        context["more_about_me"] = split_paragraphs(config.MORE_ABOUT_ME)
 
         return context
 
 
-class ImpressumView(BaseView):
+class ImpressumView(TemplateView):
     template_name = "impressum.html"
