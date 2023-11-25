074f60a5b9c9c986d1de7e820d88de041658455d
Vanda Farsad <v.farsad@orendtstudios.com>
2023-11-25 12:26:02 +0100

    fix DEBUG is type string err

--- conf/settings.py
+++ conf/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = True if os.getenv("DEBUG") == "True" else False
 
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
